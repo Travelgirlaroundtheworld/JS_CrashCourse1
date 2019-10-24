aecfe09224635d73973d4664c8a201007ed956f5
Tatjana <55509610+Travelgirlaroundtheworld@users.noreply.github.com>
2019-10-24 15:28:24 +0200

    updates homework.js

--- Homework.js
+++ Homework.js
@@ -1,38 +1,30 @@
 
-Person = class{
-    constructor(name, age){
-        this.name = name
-        this.age = age
-    }
-    greet(person){
-        console.log("Hi, I am " + this.name + "and I am going to buy " + this.phone)
-    }
+Person = class {
+ constructor(name, age) {
+   this.name = name
+   this.age = age
+ }
+ greet(phone){
+   console.log("Hi, I am " + this.name + " and I am going to buy a " + phone.name)
+ }
+ buy(phone) {
+   console.log("Yay, welcome to " + phone.name + "!")
+ }
+ pay(phone) {
+   console.log("You have to pay" + phone.price + "!")
+ }
 };
-
-
 Phone = class {
-    constructor(name, color, price){
-        this.name = name
-        this.color = color
-        this.price = price
-    }
-    buy(phone){
-        this.phone = phone
-        console.log("Yay, welcome to " + this.phone + "!")
-    }
-    pay(phone) {
-        console.log("You have to pay" + this.price + "!")
-    }
-};
-
-phone1 = new Phone("iPhone", "black", 1000)
+ constructor(name, color, price) {
+   this.name = name
+   this.price = price
+   this.color = color
+ }};
+phone1 = new Phone("iphone", "black", 1000)
 phone2 = new Phone("nokia", "silver", 100)
-phone3 = new Phone("Samsung","navy", 500)
-
+phone3 = new Phone("Samsung", "navy", 500)
 vanessa = new Person("Vanessa", 26)
 maik = new Person("Maik", 53)
-inna = new Person("Inna", 53)
-
-vanessa.buy("phone3")
-inna.buy("phone1")
-maik.buy("phone2")
+inna = new Person("Inna", 53)vanessa.greet(phone1)
+maik.greet(phone2)
+vanessa.buy(phone3)
